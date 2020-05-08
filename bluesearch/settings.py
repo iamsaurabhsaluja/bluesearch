5bdfaedbdc7cf7fe7412fc7fb428b17d509e410c
Saurabh Saluja <saurabhsaluja.connect@gmail.com>
2020-05-08 20:46:26 +0530

    Added database

--- bluesearch/settings.py
+++ bluesearch/settings.py
@@ -97,7 +97,14 @@ DATABASES = {
 }
 """
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    "default": {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
+}
+
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
