f2838d3f332b35fb6748c591710f062a39581ee0
Saurabh Saluja <saurabhsaluja.connect@gmail.com>
2020-05-10 23:07:32 +0530

    added http json response

--- discordsearch/views.py
+++ discordsearch/views.py
@@ -3,10 +3,28 @@ from discordsearch.models import Messages
 from django.views.decorators.csrf import csrf_exempt
 
 from discordsearch.services.ViewService import ViewService
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+def genericResponse( obj ):
+    response = HttpResponse( json.dumps( obj ), content_type="application/json" )
+    return response
+
+def StatusResponse( status_info = None, status = None, data = None, updated_time = '' ):
+    response = {}
+
+    response['status_info'] = status_info;
+    response['status'] = status;
+    response['updated_time'] = str(updated_time);
+    response['data'] = data
+
+    return response;
 
 @csrf_exempt
 def startEngineView( request ):
     view_service = ViewService()
     view_service.startEngine()
+
+    response = StatusResponse(status_info = "Successful", status = '1001', data = {})
+
+    return genericResponse( response )
